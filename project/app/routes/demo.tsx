83e311cacdca12a4d08d0ef15e461407ff5387cd
AlxAshl <AlxAshl@users.noreply.github.com>
2023-09-14 16:20:21 +0300

    testcom

---
+++ project/app/routes/demo.tsx
@@ -0,0 +1,9 @@
+import { Link } from "@remix-run/react";
+export default function Demo() {
+    return (
+        <>
+            <h1>DEMO PAGE</h1>
+            <Link to ="/">GO TO MAIN PAGE</Link>
+        </>
+    )
+}
